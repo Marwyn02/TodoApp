954edfa64f72808bb511822d80006a5bf56f7f17
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2023-07-06 09:04:18 +0800

    Auth0 Added

--- Todo/Todo/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Todo;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
